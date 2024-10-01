9584b3c2a15ac3d3d104c4d1a189e629e5668d32
Rostyslav Onikiienko <ronikiienko@gmail.com>
2024-10-01 15:49:42 -0700

    fix: avoid adding same highlight to page several times

--- src/lib/group-highlights-by-page.ts
+++ src/lib/group-highlights-by-page.ts
@@ -13,10 +13,17 @@ const groupHighlightsByPage = (
     if (!highlight) {
       continue;
     }
-    const pageNumbers = [
-      highlight.position.boundingRect.pageNumber,
-      ...highlight.position.rects.map((rect) => rect.pageNumber || 0),
-    ];
+
+    // Collect all page numbers that the highlight spans
+    // And add that highlight to keys corresponding to all those page numbers (also filtering out any rects from other pages)
+    // Use Set to avoid adding copy of highlight for each rect
+    const pageNumbersSet = new Set<number>();
+    pageNumbersSet.add(highlight.position.boundingRect.pageNumber);
+    for (const rect of highlight.position.rects) {
+      pageNumbersSet.add(rect.pageNumber || 0);
+    }
+    const pageNumbers = Array.from(pageNumbersSet);
+
     for (const pageNumber of pageNumbers) {
       grouped[pageNumber] ||= [];
 
@@ -33,6 +40,7 @@ const groupHighlightsByPage = (
     }
   }
 
+  console.log('grouped highlights', grouped);
   return grouped;
 }
 
